9df9d0a15000d7dbeb0c9aa9564b89f3ba0d47ce
Tomas Ye <t.ye@sap.com>
2018-07-27 21:22:31 +0200

    added dialog for chaning operators

--- app/src/main/java/com/example/i354640/numbergameproject/PlayActivity.java
+++ app/src/main/java/com/example/i354640/numbergameproject/PlayActivity.java
@@ -1,8 +1,17 @@
 package com.example.i354640.numbergameproject;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PlayActivity extends AppCompatActivity {
 
@@ -11,15 +20,57 @@ public class PlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
-        SequenceGenerator sequenceGenerator = new SequenceGenerator();
-        int[] nums = {1, 2, 3, 4,5};
-        char[] chars = {'+','-','*', '+'};
-        int result = 24;
 
-        Sequence sequence = new Sequence(result,nums,chars);
-        int evaluationResult = sequence.evaluate();
-        Toast.makeText(getApplicationContext(),"The sequence has been evaluated into: " + evaluationResult,Toast.LENGTH_LONG).show();
+
+
+
+        //Here is the onClickListener common for all operator windows
+
+        View.OnClickListener operatorOnClickListener = ( view ) -> {
+            AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
+            View mView = getLayoutInflater().inflate(R.layout.layout_select_operator, null);
+            mBuilder.setView(mView);
+            AlertDialog chooseOperatorDialog = mBuilder.create();
+            chooseOperatorDialog.show();
+            ImageView operatorImageView = findViewById(view.getId());
+
+            //in the layout the items have attribute clickable turned on!
+            chooseOperatorDialog.findViewById(R.id.selectMinusImageView).setOnClickListener(view1 -> {
+                operatorImageView.setImageResource(R.drawable.minus);
+                chooseOperatorDialog.cancel();
+            });
+
+            chooseOperatorDialog.findViewById(R.id.selectPlusImageView).setOnClickListener(view1 -> {
+                operatorImageView.setImageResource(R.drawable.plus);
+                chooseOperatorDialog.cancel();
+            });
+
+            chooseOperatorDialog.findViewById(R.id.selectTimesImageView).setOnClickListener(view1 -> {
+                operatorImageView.setImageResource(R.drawable.times);
+                chooseOperatorDialog.cancel();
+            });
+
+
+            
+        };
+
+        ImageView[] operatorImageViews = new ImageView[]{
+          findViewById(R.id.operator1ImageView),
+          findViewById(R.id.operator2ImageView),
+          findViewById(R.id.operator3ImageView)
+        };
+
+        for (ImageView operatorImageView: operatorImageViews) {
+            operatorImageView.setClickable(true);
+            operatorImageView.setOnClickListener(operatorOnClickListener);
+        }
+
+
+
+
     }
+
+
 
 
 }
